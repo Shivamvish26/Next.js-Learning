193a3ac059c9153e3efc9e486585372cdd4e169b
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2025-10-11 18:14:58 +0530

    events and hooks and use of use client

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,12 +1,43 @@
-'use client'
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  // Hooks
+  const [name, setName] = useState("Yadhav");
+  const [count, setCount] = useState(0);
+
+  {
+    /* Parameter in a function */
+  }
+  const Suraname = () => {
+    // setName("Vishwakarma");
+    setCount(count + 1);
+  };
+  const apple = (item) => alert(item);
+
+  // How to make a component in component
+  const InnerComp = () => {
+    return (
+      <>
+        <h3>Inner Function</h3>
+      </>
+    );
+  };
+
   return (
     <div className={styles.page}>
       {/* <BlogPost name={"Shubham"} /> */}
       <h1>Welcome to My Blog Page</h1>
-      <button onClick={() => alert("Hello Next.js")}>Click Me</button>
+      <h2>Total Count {count}</h2>
+      <button onClick={() => apple("Welcome to the course of Next Js")}>
+        Click Me
+      </button>
+      {/* message passed for the item as "Welcomr to the....." */}
+      <button onClick={Suraname}>Increment the Value</button>
+      {/* <InnerComp/> */}
+      {InnerComp()}
+      {/*we can call the inner comp as an function also  */}
     </div>
   );
 }
