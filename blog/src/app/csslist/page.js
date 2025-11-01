54ff9bf02f90f9383b0b4f7ba9014c44d797a989
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2025-11-01 14:09:27 +0530

    css done

--- blog/src/app/csslist/page.js
+++ blog/src/app/csslist/page.js
@@ -1,10 +1,11 @@
 "use client";
 
 import { useState } from "react";
+import outside from "../../style/outside.module.css";
 
 export default function CSSListPage() {
-
-    const [h3color, setH3color] = useState({ backgroundColor: "yellow" });
+  const [h3color, setH3color] = useState({ backgroundColor: "yellow" });
+  const [textcolor, setTextcolor] = useState("blue");
 
   return (
     <div>
@@ -13,8 +14,19 @@ export default function CSSListPage() {
         CSS List Page
       </h2>{" "}
       {/*Inline css*/}
-      <h3 className="p-3 text-dark" style={h3color}>CSS List page</h3>{/*Chaning the background color on click event*/}
-      <button onClick={()=>setH3color({backgroundColor:'red'})}>Click Me to Change</button>
+      <h3 className="p-3 text-dark" style={h3color}>
+        CSS List page
+      </h3>
+      {/*Chaning the background color on click event*/}
+      <button onClick={() => setH3color({ backgroundColor: "red" })}>
+        Click Me to Change
+      </button>
+      {/* css module code */}
+      <div className={outside.main}>
+        <h1>CSS Module</h1>
+        <p>This is a paragraph inside a CSS Module styled div.</p>
+      </div>
+      
     </div>
   );
 }
